3445b1785ce79287b25850a9915d0a9127f46e2f
MLangteau <langteauM@gmail.com>
2017-02-25 23:10:09 -0500

    working well, but not lookin' pretty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,184 +1,117 @@
 $(document).ready(function() {
 
-	var ScoreToGet = 0;
-	var ScoreGuessTot = 0;
+    var ScoreToGet = 0;
+    var ScoreGuessTot = 0;
 
-	var wins = 0;
-	var losses = 0;
-	
-	var gems = {
-				red: 	{
-						name: "red",
-						value: 0
-				},
-				blue: 	{
-						name: "blue",
-						value: 0
-				},
-				yellow: {
-						name: "yellow",
-						value: 0
-				},
-				green: {
-						name: "green",
-						value: 0
-				}
-	};
+    var wins = 0;
+    var losses = 0;
+    
+    var gems = [{
+        name: "red",
+        imgURL: "assets/images/redgem.png",
+        value: 0
+        },
+        {
+        name: "blue",
+        imgURL: "assets/images/bluegem.png",
+        value: 0
+        },
+        {
+        name: "yellow",
+        imgURL: "assets/images/yellowgem.png",
+        value: 0
+        },
+        {
+        name: "green",
+        imgURL: "assets/images/greengem.png",
+        value: 0
+        }];
 
-//	var redImg = "src": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR69L_07SDo00sGEnJymXCNLZoCwpQ3NQ6QvQfJPTXB2cyYr-_ZIA";
-//	var blueImg = "src": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR69L_07SDo00sGEnJymXCNLZoCwpQ3NQ6QvQfJPTXB2cyYr-_ZIA";
-//	var yellowImg = "src": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR69L_07SDo00sGEnJymXCNLZoCwpQ3NQ6QvQfJPTXB2cyYr-_ZIA";
-//	var greenImg = "src": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR69L_07SDo00sGEnJymXCNLZoCwpQ3NQ6QvQfJPTXB2cyYr-_ZIA";
+    function startGame() {
+    // Doing your setup 
+    //  Start and Reset function
+        console.log("Starting game:");
 
-	function startGame() {
-	//  Start and Reset function
-	
-		console.log("Starting game:");
-		}
+        ScoreToGet = genRand (19,120);
 
-	//var ScoreToGet = Math.floor(Math.random() * (120-19) + 19;
+        $('#randomGen').html(ScoreToGet);
+      
+        console.log("ScoreToGet : " + ScoreToGet);
+  
+        for (i = 0; i < gems.length; i++) {
+        // Generate gem values
+            gems [i].value = genRand (1,12);
 
-	//  THIS MY FUNCTION TO GET A RANDOM NUMBER
-	    var genRand function(minNum, maxNum) {
-	    	return Math.floor(Math.random() * (maxNum-minNum + 1)) + minNum;
-	    };
-	};
+            //  placing all of these values in the div 
+            $('<img>').attr('id', 'gem' + i )
+            .addClass('gem')
+            .data('value', gems[i].value)
+            .on('click', calculateTotal)  //This is the on-click for calculateTotal function for the gems
+            .attr('src', gems[i].imgURL)
+            .appendTo('#crystal-panel');
 
-//  THIS IS MY FUNCTION TO ******************
-
-    function gemsAdder() {
-        // body...
-        
-        $(".gems").on("click" , function(){
-
-            var user = $(this);
-     
-            var user_pick = user.data("index");
-            console.log(user);
-            console.log(user_pick);
+        };  //  end of for loop
+    };  // end of StartGame function
 
 
-            $("index").html(user_pick);
+    function calculateTotal() {
+          // logic
+        var valueOfGemClicked = $(this).data('value');
+ //     valueOfGemClicked = parseInt(valueOfGemClicked);            Unnecessary
 
-        }) //  end of on.click function for gems
+// We then add the valueOfGemClicked to the user's "ScoreGuessTot" which is a global variable.
+        ScoreGuessTot += valueOfGemClicked;
+        $('#calcScore').html(ScoreGuessTot);
 
-    }; // end of gemsAdder function
+        if (ScoreGuessTot === ScoreToGet) {
+            wins++;
+            $('#wins-on-pg').html(wins);
+            $('#losses-on-pg').html(losses);
+            alert("You win!");
+            refreshScreen();
+        }
+        else if (ScoreGuessTot > ScoreToGet) {
+            losses++;
+            $('#wins-on-pg').html(wins);
+            $('#losses-on-pg').html(losses);
+            alert("You lose!!");
+            refreshScreen();
+        }
+    };
+
+    function genRand (minNum, maxNum) {
+        var gitNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+        return gitNum;
+    };
+
+    function refreshScreen() {
+
+        ScoreGuessTot = 0;
+        ScoreToGet = genRand (19,120);
+
+        $('#calcScore').html(ScoreGuessTot);
+        $('#randomGen').html(ScoreToGet);
+      
+       for (i = 0; i < gems.length; i++) {
+            // Generate NEW gem values
+            gems [i].value = genRand (1,12);
+ 
+            // storing the above values in the object
+             $('#gem' + i).data('value', gems[i].value); 
+ 
+            console.log("refreshScreen [i]: " + i + " gems[i].value: " + gems[i].value);
+
+         };  //  end of for loop
+    };  //  end of refreshScreen function
+
 
 //  ******************    START THE GAME   ********************************************
-	startGame();
+    
+    $('#randomGen').html(ScoreToGet);
+//      $('#randomGen').text(ScoreToGet);
 
-    ScoreToGet = genRand (19,120);
+    $('#calcScore').html(ScoreGuessTot);
 
-    console.log("ScoreToGet: " + ScoreToGet);
-	
-	gems.red.value = genRand(1,12);
+    startGame();
 
-	console.log("redGemRand: " + gems.red.value);
-	gems.blue.value = genRand(1,12);
-	console.log("blueGemRand: " + gems.blue.value);
-	gems.yellow.value = genRand(1,12);
-	console.log("yellowGemRand: " + gems.yellow.value);
-	gems.green.value = genRand(1,12);
-	console.log("greenGemRand: " + gems.green.value);
-
-	$('#randomGen').html(ScoreToGet);
-
-	$('.calcScore').html(ScoreGuessTot);
-
-	$('#gem1').attr("value", gems.red.value);
-	var whatever = $('#gem1').attr("value");
-
-	alert("New Guessing Total: " + ScoreGuessTot);
-
-    if (ScoreGuessTot === ScoreToGet) {
-      alert("You win!");
-    }
-
-    else if (ScoreGuessTot >= ScoreToGet) {
-      alert("You lose!!");
-    }
-
-
-});  // end of document.ready function
-
-
-/*
-	$('#gem1').attr("value", redGemRand);
-	var whatever = $('#gem1').attr("value");
-	console.log ('regGem: ' + whatever);   // hiding from the user
-
-	$('blueGem').attr("value", blueGemRand);
-	var whatever = $('blueGem').attr("value");
-	console.log ('blueGem: ' + whatever);      // hiding from the user
-
-	$('yellowGem').attr("value", yellowGemRand);
-	var whatever = $('yellowGem').attr("value");
-	console.log ('yellowGem: ' + whatever);     // hiding from the user
-
-	$('greenGem').attr("value", greenGemRand);
-	var whatever = $('greenGem').attr("value");
-	console.log ('greenGem: ' + whatever);      // hiding from the user
-*/
-
-	//When clicked to saved to final score;
-/*  *******************************************************************  @@@@@@   ********************************
-	$("#crystal-start").on("click" , function(){ 
-	}
-
-	$('gems').on("click", function () {
-		// body...
-//		var test1 = 
-	}
-
-	if (ScoreToGet === ScoreGuessTot) {
-		alert("win");
-	}
-	else if (ScoreToGet < ScoreGuessTot){
-		alert("You Lose");
-	}
-}
-
-	$("#crystal-start").text("Crystal Start");
-    // get the math generated number
-
-    var gem_types = ["redGem","blueGem","yellowGem","greenGem"];
-
- //   var sources = ["../assets/images/redgem.png","../assets/images/yellowgem.png"];
-
-
-    $("#crystal-start").on("click" , function(){
-    //    console.log("on clicks");
-    //  $(".crystal-panel").html()
-
-//		for (var i in gem_types.length) {
-        for (var i=0; i < gem_types.length; i++) {            //   SAME AS the previous for
-            console.log("i: " + i);
-
-            var btn_multi = $("<buttons>");
-
-     //      btn_multi.text(gem_types[i]);  ///later will put the images here
-
-     //       console.log("i: " + i + " " + gem_types[i]);
-
-            $("#crystal-panel").append(btn_multi);
-
-        //  btn_multi.addClass();                               Could do it this place
-        //  btn_multi.text("put something")                     Could do it this place  
-
-            btn_multi.attr({
-                "class": "btn btn-default text-center btn-success gems",
-                "data-index": gem_types[i],        //  unique way to get info  
-              "src": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR69L_07SDo00sGEnJymXCNLZoCwpQ3NQ6QvQfJPTXB2cyYr-_ZIA"
-            });   // end of attribute
-
-        };  // end of for loop
-
-        gemsAdder(a, b);
-        
-
-//*/
- //   });  // end of on.click function
-
-
-
-
+ });  // end of document.ready function
--- gameworking.js
+++ gameworking.js
@@ -1,3 +1,245 @@
+//  bottom part is the working part at the beginning.  The top part is when it was in the index.html working
+//  in the final stages.
+
+
+$(document).ready(function() {
+
+    var ScoreToGet = 0;
+    var ScoreGuessTot = 0;
+
+    var wins = 0;
+    var losses = 0;
+    
+    var gems = [{
+        name: "red",
+        imgURL: "assets/images/redgem.png",
+        value: 0
+        },
+        {
+        name: "blue",
+        imgURL: "assets/images/bluegem.png",
+        value: 0
+        },
+        {
+        name: "yellow",
+        imgURL: "assets/images/yellowgem.png",
+        value: 0
+        },
+        {
+        name: "green",
+        imgURL: "assets/images/greengem.png",
+        value: 0
+        }];
+
+    function startGame() {
+    // Doing your setup 
+    //  Start and Reset function
+        console.log("Starting game:");
+
+        ScoreToGet = genRand (19,120);
+
+        $('#randomGen').html(ScoreToGet);
+      
+        console.log("ScoreToGet : " + ScoreToGet);
+  
+        for (i = 0; i < gems.length; i++) {
+        // Generate gem values
+            gems [i].value = genRand (1,12);
+
+            console.log("[i]: " + i + " gems[i].value: " + gems[i].value);
+
+            $('<img>').attr('id', 'gem' + i )
+            .addClass('gem')
+            .data('value', gems[i].value)
+            .on('click', calculateTotal)  //This is the on-click for calculateTotal function for the gems
+            .attr('src', gems[i].imgURL)
+            .appendTo('#crystal-panel');
+
+        };  //  end of for loop
+    };  // end of StartGame function
+
+
+    function calculateTotal() {
+          // logic
+        var valueOfGemClicked = $(this).data('value');
+
+        console.log("WHO AH!  " + $(this).data('value'));
+
+ //     valueOfGemClicked = parseInt(valueOfGemClicked);
+        console.log("valueOfGemClicked: " + valueOfGemClicked);
+// We then add the valueOfGemClicked to the user's "ScoreGuessTot" which is a global variable.
+        ScoreGuessTot += valueOfGemClicked;
+        $('#calcScore').html(ScoreGuessTot);
+
+        console.log("New ScoreGuessTot: " + ScoreGuessTot);
+//        alert("New Guessing Total: " + ScoreGuessTot);
+        
+        if (ScoreGuessTot === ScoreToGet) {
+            wins++;
+            $('#wins-on-pg').html(wins);
+            $('#losses-on-pg').html(losses);
+            alert("You win!");
+            refreshScreen();
+        }
+        else if (ScoreGuessTot > ScoreToGet) {
+            losses++;
+            $('#wins-on-pg').html(wins);
+            $('#losses-on-pg').html(losses);
+            alert("You lose!!");
+            refreshScreen();
+        }
+    };
+
+ /*      $(".crystal-image").on("click", function() {
+        
+            var crystalValue = ($(this).attr("data-index"));
+            // must parse, since it is could be considered string
+            crystalValue = parseInt(crystalValue);
+            //  then add to the ScoreGuessTot counter
+            ScoreGuessTot += crystalValue;
+            console.log(" New Score: " + ScoreGuessTot);
+*/
+
+    //  THIS MY FUNCTION TO GET A RANDOM NUMBER
+/*        var genRand2 = function(minNum,maxNum) {
+            return Math.floor(Math.random() * (maxNum -minNum + 1)) + minNum;
+        };
+
+        This is how you call that function - or something like this
+            ScoreToGet = genRand2 (19,120);
+*/    
+    function genRand (minNum, maxNum) {
+        var gitNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+        return gitNum;
+    };
+
+    function refreshScreen() {
+
+        ScoreGuessTot = 0;
+        ScoreToGet = genRand (19,120);
+
+        $('#calcScore').html(ScoreGuessTot);
+        $('#randomGen').html(ScoreToGet);
+      
+        console.log("ScoreToGet NOWWWWWWWWWWW: " + ScoreToGet);
+
+       for (i = 0; i < gems.length; i++) {
+            // Generate NEW gem values
+            gems [i].value = genRand (1,12);
+ 
+            // storing the above values in the object
+             $('#gem' + i).data('value', gems[i].value); 
+ 
+            console.log("refreshScreen [i]: " + i + " gems[i].value: " + gems[i].value);
+
+         };  //  end of for loop
+    };  //  end of refreshScreen function
+
+
+//  ******************    START THE GAME   ********************************************
+
+    
+    console.log("I am starting everything here:");
+
+    $('#randomGen').html(ScoreToGet);
+//      $('#randomGen').text(ScoreToGet);
+
+    $('#calcScore').html(ScoreGuessTot);
+    console.log("I am here:");
+
+    startGame();
+
+
+ });  // end of document.ready function
+
+
+    </script>      
+
+<!--
+
+/*        $(document).ready(function() {
+
+        //    $('').css('border', '4px solid red');
+
+          $("#red-gem").on("click" , function(){
+
+              $("#red-gem").html("red button");   // replace with gem
+                
+              console.log("clicking red");
+
+            
+          }) //  end of crystal1 function
+
+        });  // end of (document).ready(function().../
+*/
+
+/*
+
+$(document).ready(function() {
+
+$("#crystal-start").text("Crystal Start");
+    // get the math generated number
+
+    var gem_types = ["redGem","blueGem","yellowGem","greenGem"];
+
+ //   var sources = ["../assets/images/redgem.png","../assets/images/yellowgem.png"];
+
+    $("#crystal-start").on("click" , function(){
+    //    console.log("on clicks");
+    //  $(".crystal-panel").html()
+
+        for (var i=0; i < gem_types.length; i++) {
+   //         console.log("i: " + i);
+
+            var btn_multi = $("<buttons>");
+
+            btn_multi.text(gem_types[i]);  ///later will put the images here
+
+            console.log("i: " + i + " " + gem_types[i]);
+
+            $("#crystal-panel").append(btn_multi);
+
+        //  btn_multi.addClass();                               Could do it this place
+        //  btn_multi.text("put something")                     Could do it this place  
+
+            btn_multi.attr({
+                "class": "btn btn-default text-center btn-success gems",
+                "data-index": gem_types[i]        //  unique way to get info
+  //              "text": gem_types[i]
+            });   // end of attribute
+
+        };  // end of for loop
+
+        gemsAdder();
+
+    });  // end of on.click function
+
+    function gemsAdder() {
+        // body...
+        $(".gems").on("click" , function(){
+
+            var user = $(this);
+     
+
+            var user_pick = user.data("index");
+            console.log(user);
+            console.log(user_pick);
+
+            $("index").html(user_pick);
+
+        }) //  end of on.click function for gems
+
+    } // end of gemsAdder function
+
+    });  // end of document.ready function
+
+*/
+-->
+      
+
+
+
+//  *******  BOTTOM PART ********************************************************************************
 $(document).ready(function() {
 
 $("#crystal-start").text("Crystal Start");
