59413fd29191b484be80342fec6c03e5c257414d
MLangteau <langteauM@gmail.com>
2017-02-26 20:39:54 -0500

    week 4 homework crystal game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,22 +27,85 @@ $(document).ready(function() {
         value: 0
         }];
 
+    function setUpLayout() {
+
+        $('h1').css({
+                'background-color': '#D2691E',
+                'padding':'3% 3% 3% 1px',
+                'margin-left': '1%',
+                'display': 'block',
+                'font-size': '95%',
+                'width':'40%',
+                'color': 'white'
+            });  // end of css 
+
+        $('.para').css({
+                'background-color': '#FFF8DC',
+                'margin-left': '1%',
+                'display': 'block',
+                'font-size': '50%',
+                'font-family': 'Helvetica',
+                'width':'50%',
+                'padding': '2%',
+                'color': '#32CD32'
+            });  // end of css 
+
+        $('#randomGen').css({
+                    'background-color': '#32CD32',
+                    'padding-top': '5%',
+                    'padding-bottom': '5%',
+                    'padding-left': '15%',
+                    'padding-right': '15%',                    
+                    'display': 'block',
+                    'float': 'left',
+                    'margin-left': '1%'
+                    });  // end of css   
+
+        $('.winLossBox').css({
+                'background-color': '#00FFFF',
+                'width': '15%',
+                'padding-top':'1%',
+                'padding-bottom': '1%',
+                'padding-left': '1%',
+                'padding-right': '20%', 
+                'font-size': '50%',
+                'clear': 'both',
+                'display': 'inline-block'
+                });  // end of css 
+
+        $('#totalScoreText').css({
+                'background-color': '#3CB371',
+                'padding':'1%',
+                'margin-left': '1%',
+                'font-size': '60%',
+                'width':'25%'
+            });  // end of css 
+
+        $('#calcScore').css({
+                'background-color': '#3CB371',
+                'display': 'block',
+                'overflow': 'auto',
+                'clear': 'both',
+                'margin-left': '1%',
+                'margin-bottom': '1%',
+                'padding': '1% 10% 10% 1%',
+                'width':'25%'
+                 });  // end of css 
+
+    }  // end of setUpLayout function
+
     function startGame() {
     // Doing your setup 
     //  Start and Reset function
-        console.log("Starting game:");
 
         ScoreToGet = genRand (19,120);
-
         $('#randomGen').html(ScoreToGet);
-      
-        console.log("ScoreToGet : " + ScoreToGet);
-  
-        for (i = 0; i < gems.length; i++) {
-        // Generate gem values
-            gems [i].value = genRand (1,12);
+        $('#calcScore').html(ScoreGuessTot);
+        $('.winLossBox').html("Wins: " + wins + "<p>" + "<p>" +  "Losses: " + losses);
 
-            //  placing all of these values in the div 
+        // Generates initial gem values and gem sets up the gems on screen
+        for (var i = 0; i < gems.length; i++) {
+            gems [i].value = genRand (1,12);
             $('<img>').attr('id', 'gem' + i )
             .addClass('gem')
             .data('value', gems[i].value)
@@ -50,34 +113,37 @@ $(document).ready(function() {
             .attr('src', gems[i].imgURL)
             .appendTo('#crystal-panel');
 
-        };  //  end of for loop
+            $('#gem' + i).css({
+                    'width': '20%',
+            //        'margin':'10px'
+                    'margin-right':'30px'});
+
+       };  //  end of for loop
+ 
     };  // end of StartGame function
 
 
     function calculateTotal() {
-          // logic
+        // logic
         var valueOfGemClicked = $(this).data('value');
  //     valueOfGemClicked = parseInt(valueOfGemClicked);            Unnecessary
 
-// We then add the valueOfGemClicked to the user's "ScoreGuessTot" which is a global variable.
+// We then add the valueOfGemClicked to the user's "ScoreGuessTot" - global variable
         ScoreGuessTot += valueOfGemClicked;
-        $('#calcScore').html(ScoreGuessTot);
+
+        $('#calcScore').html(ScoreGuessTot);  // puts new score on the screen
 
         if (ScoreGuessTot === ScoreToGet) {
             wins++;
-            $('#wins-on-pg').html(wins);
-            $('#losses-on-pg').html(losses);
-            alert("You win!");
+             alert("You Win!!");
             refreshScreen();
         }
         else if (ScoreGuessTot > ScoreToGet) {
             losses++;
-            $('#wins-on-pg').html(wins);
-            $('#losses-on-pg').html(losses);
             alert("You lose!!");
             refreshScreen();
-        }
-    };
+        };   // end of else if
+    };  // end of calculateTotal function
 
     function genRand (minNum, maxNum) {
         var gitNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
@@ -85,33 +151,31 @@ $(document).ready(function() {
     };
 
     function refreshScreen() {
-
+// refreshes scores, I will refresh the screen at the end after it replaces the gam values
         ScoreGuessTot = 0;
         ScoreToGet = genRand (19,120);
-
-        $('#calcScore').html(ScoreGuessTot);
-        $('#randomGen').html(ScoreToGet);
-      
-       for (i = 0; i < gems.length; i++) {
+   
+       for (var i = 0; i < gems.length; i++) {
             // Generate NEW gem values
             gems [i].value = genRand (1,12);
- 
             // storing the above values in the object
-             $('#gem' + i).data('value', gems[i].value); 
- 
-            console.log("refreshScreen [i]: " + i + " gems[i].value: " + gems[i].value);
-
+            $('#gem' + i).data('value', gems[i].value); 
+            $('#gem' + i).slideToggle(1000).slideToggle(1000);
+            $('#gem' + i).css({'width': '20%','margin':'5px'});
          };  //  end of for loop
+
+// refreshes scores to screen
+        $('#calcScore').html(ScoreGuessTot);
+        $('#randomGen').html(ScoreToGet);
+        $('.winLossBox').html
+        ("Wins: " + wins + "<p>" + "<p>" +  "Losses: " + losses);
+
     };  //  end of refreshScreen function
 
 
 //  ******************    START THE GAME   ********************************************
-    
-    $('#randomGen').html(ScoreToGet);
-//      $('#randomGen').text(ScoreToGet);
 
-    $('#calcScore').html(ScoreGuessTot);
-
+    setUpLayout();
     startGame();
 
  });  // end of document.ready function
